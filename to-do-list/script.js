62abb7075becfbedd05d85c1dd661a8257e370a3
nightknight <nightknightamir@gmail.com>
2024-03-21 18:04:27 +0330

    FTP

---
+++ to-do-list/script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".btn");
+const listContainer = document.querySelector(".list-container");
+const inputvalue = input.value;
+
+function getInputData() {
+  if (inputvalue === "") {
+    alert("you must write someting");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputvalue;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "/u00d7";
+    li.appendChild(span);
+  }
+  inputvalue = "";
+  saveData();
+}
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === LI) {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === SPAN) {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+function saveData() {
+  localStorage.setItem(data, listContainer.innerHTML);
+}
+function showData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showData()
